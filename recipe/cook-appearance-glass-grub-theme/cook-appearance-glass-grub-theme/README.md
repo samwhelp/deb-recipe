9e0d4395c5c341f36a49720d8fe8b02dd6a7e8a2
samwhelp <samwhelp@users.noreply.github.com>
2024-12-15 10:04:10 +0800

    fix

---
+++ recipe/cook-appearance-glass-grub-theme/cook-appearance-glass-grub-theme/README.md
@@ -0,0 +1,10 @@
+
+
+# cook-appearance-glass-grub-theme
+
+
+
+
+## Source
+
+* samwhelp / [grub-theme-glass-remix](https://github.com/samwhelp/grub-theme-glass-remix)
---
+++ recipe/cook-appearance-glass-grub-theme/cook-appearance-glass-grub-theme/asset/installer.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+
+#set -e
+
+
+
+
+##
+## # Install Theme
+##
+
+
+
+
+##
+## ## Main / Init
+##
+
+REF_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+REF_CMD_FILE_NAME="$(basename "$0")"
+
+
+
+
+##
+## ## Main / Args
+##
+
+DEFAULT_SRCDIR="${REF_BASE_DIR_PATH}/prj"
+SRCDIR="${SRCDIR:=$DEFAULT_SRCDIR}"
+
+
+DEFAULT_DESTDIR="${REF_BASE_DIR_PATH}/pkg"
+DESTDIR="${DESTDIR:=$DEFAULT_DESTDIR}"
+
+
+
+
+##
+## ## Kde Theme / Util
+##
+
+util_copy_from_srcdir_to_desdir () {
+
+	local src_dir_path="${1}"
+	local des_dir_path="${2}"
+
+
+	echo
+	echo mkdir -p "${src_dir_path}"
+	mkdir -p "${src_dir_path}"
+
+
+	echo
+	echo mkdir -p "${des_dir_path}"
+	mkdir -p "${des_dir_path}"
+
+
+	echo
+	echo cp -rfT "${src_dir_path}" "${des_dir_path}"
+	cp -rfT "${src_dir_path}" "${des_dir_path}"
+
+
+	echo
+
+	return 0
+
+}
+
+
+
+
+##
+## ## Grub Theme / Model
+##
+
+mod_theme_install_grub_theme () {
+
+	local prj_dir_path="${prjdir}"
+	local pkg_dir_path="${pkgdir}"
+
+	local source_themes_dir_path="${prj_dir_path}"
+	local target_themes_dir_path="${pkg_dir_path}/usr/share/grub/themes/grub-theme-glass-remix"
+
+
+
+
+	echo
+	echo util_copy_from_srcdir_to_desdir "${source_themes_dir_path}" "${target_themes_dir_path}"
+	echo
+	util_copy_from_srcdir_to_desdir "${source_themes_dir_path}" "${target_themes_dir_path}"
+
+
+	echo
+	echo rm -rf "${target_themes_dir_path}/.git"
+	echo
+	rm -rf "${target_themes_dir_path}/.git"
+
+
+
+
+	echo
+
+	return 0
+
+}
+
+
+
+
+##
+## ## Model / Start
+##
+
+model_start () {
+
+
+	echo
+	echo "P: theme install"
+
+	echo
+	echo "Shell: DESTDIR=${DESTDIR}"
+
+	prjdir="${SRCDIR}"
+
+	pkgdir="${DESTDIR}"
+
+
+	echo
+	echo mkdir -p "${prjdir}"
+	mkdir -p "${prjdir}"
+
+
+	echo
+	echo mkdir -p "${pkgdir}"
+	mkdir -p "${pkgdir}"
+
+
+
+
+	mod_theme_install_grub_theme
+
+
+
+
+	echo
+
+	return 0
+
+}
+
+
+
+
+##
+## ## Main / Start
+##
+
+__main__ () {
+
+	model_start "${@}"
+
+}
+
+__main__ "${@}"
